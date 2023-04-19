c34dd40e72b07eb137fc83bb4576441dfb79f865
SATICODER123 <99791099+SATICODER123@users.noreply.github.com>
2023-04-19 20:08:15 +1000

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,39 @@
+function tic() {
+    window.location = "tictac.html"
+}
+
+function kwit() {
+    window.location = "kwitterhome.html"
+}
+
+function word() {
+    window.location = "wordle.html"
+}
+
+function math() {
+    window.location = "2048.html"
+}
+
+function flap() {
+    window.location = "flap.html"
+}
+
+function cal() {
+    window.location = "cal.html"
+}
+
+function quiz() {
+    window.location = "game_page_login.html"
+}
+
+function eng() {
+    window.location = "game_login.html"
+}
+
+function song() {
+    window.location = "rickroll.html"
+}
+
+function keyboard() {
+    window.location = "keyboard.html"
+}
